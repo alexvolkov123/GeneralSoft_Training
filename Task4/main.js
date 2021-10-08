e814f3b3141c8626a13acba83e620d83984d2b17
Sanya <Sanya@example.com>
2021-10-08 13:44:00 +0300

    Сменил работу часов со скрипта на keyframes

--- Task4/main.js
+++ Task4/main.js
@@ -4,16 +4,14 @@ function time() {
     const secondsArrow = document.querySelector('.clock_second');
     const degrees = 6;
 
-    setInterval(()=> {
-        const date = new Date();
+    const date = new Date();
 
-        const hours = date.getHours() * 30;
-        const minutes = date.getMinutes() * degrees;
-        const seconds = date.getSeconds() * degrees;
+    const hours = date.getHours() * 30;
+    const minutes = date.getMinutes() * degrees;
+    const seconds = date.getSeconds() * degrees;
 
-        hoursArrow.style.transform = `rotateZ(${hours + (minutes / 12)}deg)`;
-        minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
-        secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
-    }, 0)
+    hoursArrow.style.transform = `rotateZ(${hours + (minutes / 12)}deg)`;
+    minutesArrow.style.transform = `rotateZ(${minutes}deg)`;
+    secondsArrow.style.transform = `rotateZ(${seconds}deg)`;
 }
 time();
