eb2ea783e12fa2c52fc15d00401c32b7dc6530f9
Sanya <Sanya@example.com>
2021-10-20 15:41:40 +0300

    Закончил работу над 6-ым заданием

--- Task6/index.js
+++ Task6/index.js
@@ -1,30 +1,42 @@
 function MyArray(array) {
-    MyArray.prototype.first = function() {
-        let result = array[0];
-        return result;
+    this.array = array;
+}
+
+MyArray.prototype.first = function() {
+    let result = this.array[0];
+    return result;
+}
+
+MyArray.prototype.last = function() {
+    return this.array[this.array.length -1];
+}
+
+MyArray.prototype.skip = function(index) {
+    let resultArray = [];
+    for(let i = index ; i < this.array.length ; i++) {
+        resultArray[i - index] = this.array[i];
     }
+    return new MyArray(resultArray);
+}
 
-    MyArray.prototype.last = function() {
-        return array[array.length -1];
+MyArray.prototype.take = function(index) {
+    let resultArray = [];
+    for(let i = 0 ; i < index ; i++) {
+        resultArray[i] = this.array[i];
     }
+    return new MyArray(resultArray);
+}
 
-    MyArray.prototype.skip = function(index) {
-        let resultArray = [];
-        for(let i = index ; i < this.array.length ; i++) {
-            resultArray[i] = this.array[i];
-        }
-        return new MyArray(resultArray);
-    }
+MyArray.prototype.map = function(func) {
+    let resultArray = [];
+    resultArray = this.array.map(func);
+    return resultArray;
+}
 
-    MyArray.prototype.take = function(index) {
-        let resultArray = [];
-        for(let i = 0 ; i < index ; i++) {
-            resultArray[i] = array[i];
-        }
-        return new MyArray(resultArray);
-    }
-
-
+MyArray.prototype.filter = function(func) {
+    let resultArray = [];
+    resultArray = this.array.filter(func);
+    return resultArray;
 }
 
 
@@ -37,5 +49,5 @@ console.log(arr.skip(2));
 console.log(arr.take(2));
 console.log(arr.take(4).skip(3));
 console.log(arr.take(4).skip(2).first());
-// console.log(arr.map(item => {return item * item}));
-// console.log(arr.filter(item => {return item % 2}));
+console.log(arr.map(item => {return item * item}));
+console.log(arr.filter(item => {return item % 2}));
